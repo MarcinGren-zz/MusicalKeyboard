68be4b4a226ad8b4b3a8fab44fe46a73514cdda9
Marcin Gren <marcin.gren@gmail.com>
2018-08-02 18:51:16 +0200

    paper implemented

--- patatapclone.js
+++ patatapclone.js
@@ -1,12 +1,11 @@
-import {Paper} from '../paper/dist/paper-full.js';
-
-// Create a Paper.js Path to draw a line into it:
-var path = new paper.Path();
-// Give the stroke a color
-path.strokeColor = 'black';
-var start = new paper.Point(100, 100);
-// Move to start and draw a line from there
-path.moveTo(start);
-// Note the plus operator on Point objects.
-// PaperScript does that for us, and much more!
-path.lineTo(start + [ 100, -50 ]);
+paper.install(window);
+window.onload = function() {
+	// Setup directly from canvas id:
+	paper.setup('myCanvas');
+	var path = new paper.Path();
+	path.strokeColor = 'black';
+	var start = new paper.Point(100, 100);
+	path.moveTo(start);
+	path.lineTo(start.add([ 200, -50 ]));
+	view.draw();
+}
