8be2d4f7822e9970edbcb9819b45ce55344ce6fa
Marcin Gren <marcin.gren@gmail.com>
2018-08-02 23:39:54 +0200

    adding circle on keypress

--- patatapclone.js
+++ patatapclone.js
@@ -1,8 +1,4 @@
-var path = new Path()
-path.strokeColor = 'white'
-var start = new Point(0, 100)
-path.moveTo(start)
-path.lineTo(start + [500, -50])
-
-var myCircle = new Path.Circle(new Point(100, 70), 50)
-myCircle.fillColor = 'pink'
+function onKeyDown(event) {
+	console.log(event.key)
+	var newCircle = new Path.Circle(new Point(100, 100), 10).fillColor = 'pink'
+}
