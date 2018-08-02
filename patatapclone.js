6ff4571c7d3b9fb1968c22e3fdbf61e2073ded42
Marcin Gren <marcin.gren@gmail.com>
2018-08-03 00:03:19 +0200

    added hue changing on every frame for all circles

--- patatapclone.js
+++ patatapclone.js
@@ -1,9 +1,19 @@
-function onKeyDown(event) {
-	console.log(event.key)
+var listOfCircles = []
+
+function onKeyDown(e) {
+	console.log(e.key)
 
 	var maxPoint = new Point(view.size.width, view.size.height)
 	var randomPlace = Point.random()
 	var newPoint = maxPoint * randomPlace
-	var newCircle = new Path.Circle(newPoint, 10).fillColor = 'pink'
-	
+	var newCircle = new Path.Circle(newPoint, 10)
+	newCircle.fillColor = 'pink'
+	listOfCircles.push(newCircle)
+
+}
+
+function onFrame(e) {
+	listOfCircles.forEach(function (elem) {
+		elem.fillColor.hue += 1
+	})
 }
