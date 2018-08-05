a75181011faf64cb484745bf3b479b59de6341e6
Marcin Gren <marcin.gren@gmail.com>
2018-08-05 10:41:52 +0200

    dictionary access fix

--- patatapclone.js
+++ patatapclone.js
@@ -1,19 +1,19 @@
 var listOfCircles = []
-
-// var sound = new Howl({
-// 	src: ['sounds/bubbles.mp3', 'sounds/bubbles.ogg']
-// })
-
 var keyDict = {
-	a: {color: "purple", sound: getSound()}
+	a: {color: "purple", sound: getSound('bubbles')}
 }
 
 function getSound(fileName) {
-	return new Howl({src: ['sounds/bubbles.mp3', 'sounds/bubbles.ogg']})
+	return new Howl({src: ["sounds/" + fileName + ".mp3", "sounds/" + fileName + ".ogg"]})
 }
+// paper.js cannot parse the below. Weird.
+// function getSound(fileName) {
+// 	return new Howl({src: [`sounds/${fileName}.mp3`, `sounds/${fileName}.ogg`]})
+// }
 
 function onKeyDown(e) {
 	console.log(e.key)
+	console.log(keyDict[e.key])
 
 	var maxPoint = new Point(view.size.width, view.size.height)
 	var randomPlace = Point.random()
@@ -21,8 +21,7 @@ function onKeyDown(e) {
 	var newCircle = new Path.Circle(newPoint, 100)
 	newCircle.fillColor = 'pink'
 	listOfCircles.push(newCircle)
-	keyDict.a.sound.play()
-	// sound.play()
+	keyDict[e.key].sound.play()
 }
 
 function onFrame(e) {
